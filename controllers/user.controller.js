a7451b3515d17871a192cf55912c9f396f2f8a5d
sverma507 <sverma70568@gmail.com>
2024-10-22 10:41:15 +0530

    new

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -88,7 +88,10 @@ export const login = async (req, res) => {
             posts: populatedPosts
         }
 
-        return res.cookie('token', token, { httpOnly: true, sameSite: 'Lax', maxAge: 1 * 24 * 60 * 60 * 1000 }).json({
+        return res.cookie('token', token, { httpOnly: true,  
+            sameSite: 'None',
+            secure: true, 
+            maxAge: 1 * 24 * 60 * 60 * 1000 }).json({
             message: `Welcome back ${user.username}`,
             success: true,
             user
