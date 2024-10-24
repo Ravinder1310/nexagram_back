dcc3070e8bf546a4b46fcbae1c72f06cfb2ffb2d
Ravinder1310 <105969256+Ravinder1310@users.noreply.github.com>
2024-10-24 14:01:03 +0530

    added

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-    origin: process.env.URL || "https://nexfront.rscjewells.shop",
+    origin: [process.env.URL || "https://nexfront.rscjewells.shop", "http://localhost:5173"],
     credentials: true
 }
 app.use(cors(corsOptions));
